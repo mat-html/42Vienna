124af21b771bfb026452b05de3da9f3450fbef71
Jovan Matic <jomatic@student.42vienna.com>
2026-02-16 19:18:43 +0100

    first commit

---
+++ src/main.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jomatic <jomatic@student.42vienna.com      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/16 16:01:47 by jomatic           #+#    #+#             */
+/*   Updated: 2026/02/16 17:51:52 by jomatic          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/f.h"
+
+int	main(int acc, char **av)
+{
+	if (ac == 1)
+		return 1;
+	if(ac != 2)
+		// argument is the file that needs to be handeld
+	generate_map(av);
+}
