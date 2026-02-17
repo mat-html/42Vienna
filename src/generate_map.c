14e8ea44b390580ea73929862e4ba70c7e84eff0
Jovan Matic <jomatic@student.42vienna.com>
2026-02-17 16:16:03 +0100

    deleted unnecessary files

--- src/generate_map.c
+++ src/generate_map.c
@@ -6,10 +6,16 @@
 /*   By: jomatic <jomatic@student.42vienna.com      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/16 16:03:56 by jomatic           #+#    #+#             */
-/*   Updated: 2026/02/16 17:36:55 by jomatic          ###   ########.fr       */
+/*   Updated: 2026/02/17 15:32:17 by jomatic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/f.h"
 
+void ft_generate_map(char **map)
+{
+	char **map_arr;
 
+	map_arr = ft_read_map(map);
+
+}
